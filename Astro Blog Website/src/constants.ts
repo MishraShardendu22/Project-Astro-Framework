8db869f418e0200756e78da3029d6309d6e91547
Shardendu Mishra <shardendumishra01@gmail.com>
2024-10-07 14:02:28 +0530

    Making The Personal Blogging Website With Astro And TypeScript Base Completed Learning About Astro !!

---
+++ Astro Blog Website/src/constants.ts
@@ -0,0 +1,21 @@
+export const SITE_TITLE = 'Mishra Blog`s It';
+export const SITE_DESCRIPTION = 'Articles, stories and Learning From Mishra Shardendu';
+export const HOMEPAGE_ARTICLE_LIMIT = 6;
+export const ARTICLES_PER_PAGE = 6;
+
+
+
+// Maybe I'll Use this Later
+// export const ABOUT_ME = `
+// 👋 Hi, I’m Shardendu Mishra!
+
+// - **Education:** Currently a student in the 2023-2027 batch at IIIT Dharwad, studying Computer Science Engineering (CSE).
+// - **What I Do:**
+//   - Full-stack developer 💻
+//   - Tech enthusiast who loves coding and programming
+//   - Aspiring entrepreneur with big dreams 🚀
+//   - Aiming to become a red coder before I graduate 🎯
+// - **Where I’m From:** Originally from Kanpur, Uttar Pradesh.
+// - **Schooling:** Went to Delhi Public School Kalyanpur, Kanpur.
+// - **Fun Fact:** Yes, I’m a proud Dipsite! 🎓
+// `;
